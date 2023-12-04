df4ee1670bedb63a32cdf60ca523263836c5994e
Nanai Jua <me@n7i.dev>
2023-12-04 10:02:53 +0900

    feat(utils): add `Exact` for using on collecting exactly one

---
+++ src/utils.rs
@@ -0,0 +1,21 @@
+pub struct Exact<T>(T);
+
+impl<T> Exact<T> {
+    pub fn into_inner(self) -> T { self.0 }
+}
+
+impl<T> FromIterator<T> for Exact<T> {
+    fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
+        let mut iter = iter.into_iter();
+
+        let Some(item) = iter.next() else {
+            panic!("unexpected end of iterator")
+        };
+
+        let None = iter.next() else {
+            panic!("unexpected one more item of iterator")
+        };
+
+        Exact(item)
+    }
+}

76d0c74d4d7f86f1d2ae141b046f13a8d72bc551
Nanai Jua <me@n7i.dev>
2023-12-06 12:19:17 +0900

    feat(base::bigint): replace implementation of `u256` and `u512`

--- src/base/bigint.rs
+++ src/base/bigint.rs
@@ -1,125 +1,59 @@
-use crate::base::astr;
+// FIXME: why cannot available `::from_be_bytes` on using `decl_macro`?
+macro_rules! uint {
+    ($name:ident,as $len:expr) => {
+        #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+        pub struct $name([u8; $len]);
 
-#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct u256(u128, u128);
+        impl $name {
+            pub fn from_be_bytes(bytes: [u8; $len]) -> Self { Self(bytes) }
 
-impl core::fmt::Debug for u256 {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        todo!("it's so difficult")
-    }
-}
-
-impl From<(u128, u128)> for u256 {
-    fn from((lhs, rhs): (u128, u128)) -> Self { u256(lhs, rhs) }
-}
-
-impl From<u256> for (u128, u128) {
-    fn from(u256(lhs, rhs): u256) -> Self { (lhs, rhs) }
-}
-
-impl serde::Serialize for u256 {
-    fn serialize<S: serde::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
-        use core::fmt::Write;
-
-        let Self(lhs, rhs) = self;
-
-        // TODO: replace fixed-length string on stack
-        let mut s = astr::<64>::new();
-
-        write!(&mut s, "{:032x}", lhs).unwrap();
-        write!(&mut s, "{:032x}", rhs).unwrap();
-
-        serializer.serialize_str(s.as_str())
-    }
-}
-
-impl<'de> serde::Deserialize<'de> for u256 {
-    fn deserialize<D: serde::Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
-        let bytes = <&str>::deserialize(deserializer)?.as_bytes();
-
-        if bytes.len() != 64 {
-            return Err(serde::de::Error::invalid_length(bytes.len(), &"64"));
+            pub fn to_be_bytes(&self) -> [u8; $len] { self.0 }
         }
 
-        let (lhs, rhs) = bytes
-            .array_chunks::<64>()
-            .collect::<crate::utils::Exact<&[_; 64]>>()
-            .into_inner()
-            .split_at(32);
-
-        let lhs = unsafe { core::str::from_utf8_unchecked(lhs) };
-        let rhs = unsafe { core::str::from_utf8_unchecked(rhs) };
-
-        let lhs = u128::from_str_radix(lhs, 16).map_err(serde::de::Error::custom)?;
-        let rhs = u128::from_str_radix(rhs, 16).map_err(serde::de::Error::custom)?;
-
-        Ok(Self(lhs, rhs))
-    }
-}
-
-// FIXME: generalize integer implements
-#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct u512(u256, u256);
-
-impl core::fmt::Debug for u512 {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        todo!("it's so difficult")
-    }
-}
-
-impl From<(u256, u256)> for u512 {
-    fn from((lhs, rhs): (u256, u256)) -> Self { u512(lhs, rhs) }
-}
-
-impl From<u512> for (u256, u256) {
-    fn from(u512(lhs, rhs): u512) -> Self { (lhs, rhs) }
-}
-
-impl serde::Serialize for u512 {
-    fn serialize<S: serde::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
-        use core::fmt::Write;
-
-        let Self(u256(llhs, lrhs), u256(rlhs, rrhs)) = self;
-
-        // TODO: replace fixed-length string on stack
-        let mut s = astr::<128>::new();
-
-        write!(&mut s, "{:032x}", llhs).unwrap();
-        write!(&mut s, "{:032x}", lrhs).unwrap();
-        write!(&mut s, "{:032x}", rlhs).unwrap();
-        write!(&mut s, "{:032x}", rrhs).unwrap();
-
-        serializer.serialize_str(s.as_str())
-    }
-}
-
-impl<'de> serde::Deserialize<'de> for u512 {
-    fn deserialize<D: serde::Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
-        let bytes = <&str>::deserialize(deserializer)?.as_bytes();
-
-        if bytes.len() != 128 {
-            return Err(serde::de::Error::invalid_length(bytes.len(), &"64"));
+        impl core::fmt::Debug for $name {
+            fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+                <Self as core::fmt::Display>::fmt(self, f)
+            }
         }
 
-        let (lhs, rhs) = bytes
-            .array_chunks::<128>()
-            .collect::<crate::utils::Exact<&[_; 128]>>()
-            .into_inner()
-            .split_at(64);
+        impl core::fmt::Display for $name {
+            fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+                todo!("it's so difficult")
+            }
+        }
 
-        let (llhs, lrhs) = lhs.split_at(32);
-        let (rlhs, rrhs) = rhs.split_at(32);
+        impl core::fmt::LowerHex for $name {
+            fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+                self.0
+                    .iter()
+                    .map(|byte: &u8| [(byte & 0b1111_0000) >> 4, (byte & 0b0000_1111) >> 0])
+                    .flatten()
+                    .skip_while(|u4| *u4 == 0)
+                    .map(|u4: u8| (u4 + u4 / 10 * (b'a' - (b'9' + 1)) + b'0') as char)
+                    .try_for_each(|c| write!(f, "{c}"))
+            }
+        }
 
-        let llhs = unsafe { core::str::from_utf8_unchecked(llhs) };
-        let lrhs = unsafe { core::str::from_utf8_unchecked(lrhs) };
-        let rlhs = unsafe { core::str::from_utf8_unchecked(rlhs) };
-        let rrhs = unsafe { core::str::from_utf8_unchecked(rrhs) };
+        impl serde::Serialize for $name {
+            fn serialize<S: serde::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+                use core::fmt::Write;
 
-        let llhs = u128::from_str_radix(llhs, 16).map_err(serde::de::Error::custom)?;
-        let lrhs = u128::from_str_radix(lrhs, 16).map_err(serde::de::Error::custom)?;
-        let rlhs = u128::from_str_radix(rlhs, 16).map_err(serde::de::Error::custom)?;
-        let rrhs = u128::from_str_radix(rrhs, 16).map_err(serde::de::Error::custom)?;
+                let mut s = crate::base::astr::<{ $len * 2 }>::new();
+                write!(&mut s, "{self:0w$x}", w = $len * 2).unwrap();
 
-        Ok(Self(u256(llhs, lrhs), u256(rlhs, rrhs)))
-    }
+                serializer.serialize_str(s.as_str())
+            }
+        }
+
+        impl<'de> serde::Deserialize<'de> for $name {
+            fn deserialize<D: serde::Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+                let bytes = <&str>::deserialize(deserializer)?.as_bytes();
+
+                Ok(todo!())
+            }
+        }
+    };
 }
+
+uint! { u256, as 32 }
+uint! { u512, as 64 }
